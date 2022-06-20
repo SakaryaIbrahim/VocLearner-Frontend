aabdd509dfb875be47a17102fabdfa75eddde445
IkhrazneMohamed <Mohamed.Ikhrazne@Student.HTW-Berlin.de>
2022-06-20 20:33:11 +0200

    get data from api

--- src/views/PlayView.vue
+++ src/views/PlayView.vue
@@ -1,9 +1,11 @@
 <template>
-  <div>
+  <div @load>
     <h2></h2>
     <input v-model="testAnswer" type="text" id="vocTest">
     <button id="submit" @click="checkTheWord">Submit</button>
-    <div></div>
+    <div id="right-answer" v-bind:style="{display: displayRight}">Congra! correct answer</div>
+    <div id="false-answer" v-bind:style="{display: displayWrong}">Sorry! Wrong answer</div>
+    <button id="play-again" @click="playAgain">Play again</button>
   </div>
 </template>
 
@@ -14,22 +16,49 @@ export default {
   data () {
     return {
       wordTotranslate: '',
-      testAnswer: ''
+      testAnswer: '',
+      displayRight: '',
+      displayWrong: ''
     }
   },
   methods: {
     checkTheWord () {
-      let helpValue = false
       if (this.wordTotranslate === this.testAnswer) {
-        // eslint-disable-next-line no-unused-vars
-        helpValue = true
+        this.displayRight = 'inline'
+      } else {
+        this.displayWrong = 'inline'
       }
-      return helpValue
+    },
+    playAgain () {
+      this.displayRight = ''
+      this.displayWrong = ''
     }
+  },
+  beforeMount () {
+    const myHeader = new Headers()
+    myHeader.append('Accept','application/json')
+    myHeader.append('Content-type','application/json')
+    const requestOptions = {
+      methods: 'GET',
+      headers: myHeader,
+      redirect: 'follow'
+    }
+    fetch('http://localhost:8080/words', requestOptions)
+      .then(response => response.json)
+      .catch(error => console.log('error', error))
   }
 }
 </script>
 
 <style scoped>
+#right-answer,#false-answer{
+  display: none;
+}
 
+#right-answer{
+  color: green;
+}
+#false-answer{
+  color: red;
+}
 </style>
