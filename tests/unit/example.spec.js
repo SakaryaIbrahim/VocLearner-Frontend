fff6d3ae4a10d46f0d086d6dc31b12ce56e13d68
SakaryaIbrahim <ibrahim.sakarya2002@gmail.com>
2022-07-04 16:29:55 +0200

    Löschen oon Komponententest

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,1 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'hello world'
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
-  })
-})
